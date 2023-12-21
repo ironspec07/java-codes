561b290fe66a9d1c0ebbace4703d7db661d0c102
ironspec07 <ironspec07@gmail.com>
2023-12-21 17:53:52 +0530

    Day 4: Arrays

---
+++ practice/DupliArray.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class DupliArray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("What's the size of array");
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        System.out.println("Enter elements of array");
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i+1; j < arr.length; j++) {
+                if (arr[i]==arr[j]) {
+                    System.out.println("true");
+                }
+            }
+        }
+        sc.close();
+    }
+}
