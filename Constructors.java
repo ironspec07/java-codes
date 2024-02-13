ab25ac22effde1416903bc7d4042358cf75b5b41
ironspec07 <ironspec07@gmail.com>
2024-02-13 21:49:43 +0530

    Day 12 : Constructors.

--- Constructors.java
+++ Constructors.java
@@ -1,10 +1,17 @@
-// every time you create a instance variable make it private. 
-// private variables are only accessable inside same class.
-
-// people should be able to acess the private variables but not directly.
 class Human{
     private int age ;
     private String name;
+
+    public Human(){ // Default constructor , never returns anything. 
+        // even if you don't call aconstructor it will be called every time you create a object.
+        age = 21;
+        name = "Adi";
+        
+    }
+    public Human(int age , String name){ // Parameterized constructor.
+        this.age = age;
+        this.name = name;
+    }
 
     public int getAge(){ //getter
 
@@ -23,16 +30,20 @@ class Human{
     }
 }
 
-public class Encapsulation {
+public class Constructors {
     public static void main(String[] args) {
-        
+
         Human obj = new Human();
+        Human obj1 = new Human(17,"Suhu");
         // obj.age = 21;
         // obj.name = "Adi";
-        obj.setAge(21);
-        obj.setName("Adi");
+        // obj.setAge(21);
+        // obj.setName("Adi");
         String name = obj.getName();
         int age = obj.getAge();
+        String name1 = obj1.getName();
+        int age1 = obj1.getAge();
         System.out.println("Name: "+name +"\n" +"Age: "+age);
+        System.out.println("Name: "+name1 +"\n" +"Age: "+age1);
     }
 }
